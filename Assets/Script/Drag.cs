5dfc2e39841730c3acc63ddd4b181af7d0d7f222
Dmitry <l.dimka@gmail.com>
2021-05-12 22:42:23 +0300

    Drag

--- Assets/Script/Drag.cs
+++ Assets/Script/Drag.cs
@@ -1,17 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Drag : MonoBehaviour
+
+[RequireComponent(typeof(CanvasGroup))]
+
+public class Drag : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    RectTransform rectTransform;
+    Canvas canvas;
+    CanvasGroup canvasGroup;
+
+    void Awake()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 0.5f;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1f;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
     {
         
     }
